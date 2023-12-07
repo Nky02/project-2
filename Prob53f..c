61eec45db13759ae5056071d09ad7f4c0b11f57b
Nics <142889497+Nky02@users.noreply.github.com>
2023-12-07 08:16:12 +0800

    Update Prob53f..c

--- Prob53f..c
+++ Prob53f..c
@@ -1,8 +1,9 @@
-/*THis program calculates and determines a  student's grade by reading three test scores
-  Written by: Nicky Shane Estrada
-  Date: November 22, 2023*/
+/* This program calculates and determines a student's grade by reading three test scores
+   Written by: Nicky Shane Estrada
+   Date: November 22, 2023*/
 
 #include <stdio.h>
+
 // function declaration
 void scoreInput(double *score1, double *score2, double *score3);
 char calcGrade(double score1, double score2, double score3);
@@ -10,7 +11,7 @@ void printRes(char grade);
 
 int main()
 {
-  // local declarartion
+  // local declaration
   double score1, score2, score3;
   char grade;
 
@@ -26,59 +27,44 @@ int main()
   return 0;
 }
 
-// function definition: input
+// function definition of input
 void scoreInput(double *score1, double *score2, double *score3)
 {
-  printf("Enter three test score separated by spaces(1-100): ");
+  printf("Enter three test scores separated by spaces (1-100): ");
   scanf("%lf %lf %lf", score1, score2, score3);
 }
 
-// function definition: calculation and process
+// function definition of calculation and process
 char calcGrade(double score1, double score2, double score3)
 {
-  double totalScore, average;
+  double totalScore = score1 + score2 + score3;
+  double average = totalScore / 3;
   char grade;
 
-  totalScore = score1 + score2 + score3;
-  average = totalScore / 3;
-
   if (average >= 90)
-  {
     grade = 'A';
-  }
-
   else if (average >= 70 && average < 90)
   {
     if (score3 > 90)
-    {
       grade = 'A';
-    }
     else
-    {
       grade = 'B';
-    }
   }
   else if (average >= 50 && average < 70)
   {
     double avgScore2andScore3 = (score2 + score3) / 2;
     if (avgScore2andScore3 > 70)
-    {
       grade = 'C';
-    }
     else
-    {
       grade = 'D';
-    }
   }
   else if (average < 50)
-  {
     grade = 'F';
-  }
 
   return grade;
 }
 
-// function definition: Output
+// function definition of  Output
 void printRes(char grade)
 {
   printf("The student's grade based on the three test scores is %c\n", grade);
