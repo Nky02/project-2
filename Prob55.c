d797bf1674bc24ec2be88ada45705531d9b19102
Nics <142889497+Nky02@users.noreply.github.com>
2023-12-07 08:18:13 +0800

    Update Prob55.c

--- Prob55.c
+++ Prob55.c
@@ -1,11 +1,12 @@
-/*THis program determines a quadrant given a user-input angle
-  Written by: Nicky Shane Estrada
-  Date: November 22, 2023*/
+/* This program determines a quadrant given a user-input angle
+   Written by: Nicky Shane Estrada
+   Date: November 22, 2023*/
 
 #include <stdio.h>
 
+// function declarations
 void getAngle(double *angle);
-const char *validateAngle(double angle);
+const char *validateAngle(int angle);
 
 int main()
 {
@@ -15,18 +16,13 @@ int main()
 
     // function calls
     getAngle(&angle);
-
     quadrantNum = validateAngle(angle);
 
     // output
     if (quadrantNum != NULL)
-    {
-        printf("The given angle is in  %s\n", quadrantNum);
-    }
+        printf("The given angle is in %s\n", quadrantNum);
     else
-    {
         printf("Invalid input\n");
-    }
 
     return 0;
 }
@@ -38,46 +34,38 @@ void getAngle(double *angle)
     scanf("%lf", angle);
 }
 
-const char *validateAngle(double givenAngle)
+const char *validateAngle(int givenAngle)
 {
     const char *quadrantNum;
 
+    givenAngle %= 360; // Using modulo operator
+
     if (givenAngle == 0)
-    {
         quadrantNum = "Positive X-axis";
-    }
+
     else if (givenAngle == 90)
-    {
         quadrantNum = "Positive Y-axis";
-    }
+
     else if (givenAngle == 180)
-    {
         quadrantNum = "Negative X-axis";
-    }
+
     else if (givenAngle == 270)
-    {
         quadrantNum = "Negative Y-axis";
-    }
+
     else if (givenAngle > 0 && givenAngle < 90)
-    {
-        quadrantNum = "quadrant number I";
-    }
+        quadrantNum = "Quadrant number I";
+
     else if (givenAngle > 90 && givenAngle < 180)
-    {
-        quadrantNum = "quadrant number II";
-    }
+        quadrantNum = "Quadrant number II";
+
     else if (givenAngle > 180 && givenAngle < 270)
-    {
-        quadrantNum = "quadrant number III";
-    }
+        quadrantNum = "Quadrant number III";
+
     else if (givenAngle > 270 && givenAngle < 360)
-    {
-        quadrantNum = "quadrant number IV";
-    }
+        quadrantNum = "Quadrant number IV";
+
     else
-    {
         quadrantNum = "Invalid";
-    }
 
     return quadrantNum;
 }
